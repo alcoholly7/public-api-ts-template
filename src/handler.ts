91ef69a1af2731cc4e6310d9972facf29c664565
Holly Liu <mistladylan@gmail.com>
2023-08-01 11:27:20 +1000

    update

--- src/handler.ts
+++ src/handler.ts
@@ -1,14 +1,7 @@
-import { S3 } from "aws-sdk"
-import axios from "axios"
-
 // From https://docs.aws.amazon.com/cdk/latest/guide/serverless_example.html
 const handler = async function (event: any, context: any) {
     console.log(`Event: ${JSON.stringify(event)}`)
     console.log("Successfully invoked the public API")
-
-    const privateAPIUrl = "https://b8v7mdb233.execute-api.ap-southeast-2.amazonaws.com/beta/"
-    const response = await axios.get(privateAPIUrl)
-    console.log("Response:", response.data)
 
     return {
         statusCode: 200,
